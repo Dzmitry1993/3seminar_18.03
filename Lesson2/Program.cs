7d7bef9b2b28c488c48f039b369fa878587bd382
Dzmitry <dmitrypetrachkov93@gmail.com>
2023-04-15 23:09:07 +0300

    Zadacha3

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -5,7 +5,7 @@
 782 -> 8
 918 -> 1
 */
-
+/*
 int Prompt (string message)
 {
 
@@ -25,3 +25,48 @@ if (number < 100 || number >= 1000)
 Console.WriteLine($"Введённое число `{number}`");
 int secondRank = number / 10 % 10;
 Console.WriteLine($"Вторая цифра`{secondRank}`");
+*/
+
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+int Prompt (string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result =Convert.ToInt32(value);
+    return result;
+    
+}
+
+int GetThirdRank (int number)
+{
+
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine ("Третьей цифры нет");
+        return false;
+    }
+
+    return true;
+}    
+    int number = Prompt("Введите число > ");
+    if (ValidateNumber(number))
+    {
+        Console.WriteLine(GetThirdRank(number));
+    }
