bd760a37a85aac23c474cc8c27ea14e9e015eec9
Dzmitry <dmitrypetrachkov93@gmail.com>
2023-04-15 23:10:43 +0300

    finish

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -35,6 +35,7 @@ Console.WriteLine($"Вторая цифра`{secondRank}`");
 
 32679 -> 6
 */
+/*
 
 int Prompt (string message)
 {
@@ -70,3 +71,51 @@ bool ValidateNumber(int number)
     {
         Console.WriteLine(GetThirdRank(number));
     }
+
+    */
+
+  /*  Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+bool isWeekend(int weekDay)
+{
+    if (weekDay > 5)
+    {
+        return true;
+    }
+    return false;
+}
+bool ValidateWeekday (int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели!");
+    return false;
+}
+int weekDay = Prompt("Введите день недели >");
+if (ValidateWeekday(weekDay))
+{
+    if (isWeekend (weekDay))
+
+{
+    Console.WriteLine("Наконец-то выходной");
+}
+else
+{
+    Console.WriteLine("Придётся поработать");
+}
+}
