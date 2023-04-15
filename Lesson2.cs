4a02d95b6a4a1c68cfd7ad5f5ce4581cc787ce37
Dzmitry <dmitrypetrachkov93@gmail.com>
2023-04-15 21:35:53 +0300

    zamena

--- Lesson2.cs
+++ Lesson2.cs
@@ -6,3 +6,22 @@
 918 -> 1
 */
 
+int Prompt (string message)
+{
+
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+
+}
+
+int number = Promt ("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine ("Вы ввели не трёхзначное число, пожалуйста повторите ввод");
+    return;
+}
+Console.WriteLine($"Введённое число `{number}`");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра`{secondRank}`");
