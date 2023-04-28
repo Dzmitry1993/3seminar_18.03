dc4d9989f33a25debf5fb0d3457ea3095e2bde1c
Dzmitry <dmitrypetrachkov93@gmail.com>
2023-04-28 23:13:36 +0300

    zadacha34.2

--- Lesson5/Program.cs
+++ Lesson5/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Введите размер массива");
+﻿
+/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.*/
+
+
+
+Console.WriteLine("Введите размер массива");
 int size = Convert.ToInt32(Console.ReadLine());
 int[] numbers = new int[size];
 FillArrayRandomNumbers(numbers);
